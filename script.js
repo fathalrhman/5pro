4f663c63291e86818fb0a7b3ec2517f01c64b29e
fathalrhman <102386274+fathalrhman@users.noreply.github.com>
2022-03-25 21:49:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+window.onscroll =() =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+}
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.toggle('fa-times');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
